432ced36399ca113dc7abe996cfc5c51e195d553
Aika586 <baatirbekovna.6@mail.ru>
2021-11-29 18:22:09 +0600

    header component built

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
+import Title from './Title';
 
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <Title/>
       
     </div>
   );
---
+++ src/Title.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './title.css'
+
+const Title=()=>{
+    return(
+        <div className="Title">
+
+        <h1>CHICAGOTOUR </h1>
+        <div className=" Title-Subtitle"> Chicago Places to Visit</div>
+
+        </div>
+    )
+}
+
+
+export default Title
